c2e1b6c725c88397159d3e6ab88dd515ead50331
Marcelo Romulo Fernandes <persapiens@gmail.com>
2025-01-26 21:51:00 -0300

    feat: simple insert and list owners

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'account';
+  title = 'account'
+
+  name = ''
+  names: Array<string> = []
+
+  insert() {
+    this.names.push(this.name)
+  }
+
+  remove(nameToRemove: string) {
+    this.names = this.names.filter(internalName => internalName !== nameToRemove)
+  }
 }
