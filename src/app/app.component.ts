a7291610df3e2b25cc65672f7f8754c01f041f61
Marcelo Romulo Fernandes <persapiens@gmail.com>
2025-01-28 12:49:48 +0000

    feat: implementing insert and list of names using primeng components. not finished yet.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,56 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, FormsModule, ButtonModule, TableModule, InputTextModule],
+  //templateUrl: './app.component.html',
+  template: `
+    <main class="main">
+      <div class="content">
+        <div class="left-side">
+          <h1>Hello, {{ name }}</h1>
+
+          <label for="name">Name:</label>
+          <input pInputText [(ngModel)]="name">
+          <p-button label="Insert" (onClick)="insert()" />
+
+          <p-table [value]="names" >
+            <ng-template #header>
+              <tr>
+                  <th>Name</th>
+                  <th>Remove</th>
+              </tr>
+          </ng-template>
+            <ng-template #body let-item>
+                <tr>
+                    <td>{{ item }}</td>
+                    <td><p-button label="Remove" (onClick)="remove(item)" /></td>
+                </tr>
+            </ng-template>
+          </p-table>
+        </div>  
+      </div>
+    </main>
+
+    <router-outlet />
+  `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'account';
+  name = ''
+
+  names: Array<string> = []
+
+  insert() {
+    this.names.push(this.name)
+  }
+
+  remove(item: string) {
+    this.names = this.names.filter(internalName => internalName !== item)
+  }
 }
