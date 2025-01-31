4d5fa5bbf98ff8b2045d0542ad7e38af540a9924
Marcelo Romulo Fernandes <persapiens@gmail.com>
2025-01-31 16:58:31 +0000

    feat: adding divider between insert and list

--- src/app/owner-crud.component.ts
+++ src/app/owner-crud.component.ts
@@ -4,13 +4,16 @@ import { OwnerInsertComponent } from "./owner-insert.component";
 import { OwnerListComponent } from './owner-list.component';
 import { PanelModule } from 'primeng/panel';
 import { Owner } from './owner';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'owner-crud',
-  imports: [FormsModule, OwnerInsertComponent, OwnerListComponent, PanelModule],
+  imports: [FormsModule, OwnerInsertComponent, OwnerListComponent, PanelModule, DividerModule],
   template: `
     <p-panel header="Owner">
       <owner-insert (insertOutEvent)="insert($event)"></owner-insert>
+
+      <p-divider />
 
       <owner-list (removeOutEvent)="remove($event)" [ownersList]="owners"></owner-list>
     </p-panel>
