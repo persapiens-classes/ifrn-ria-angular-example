3783efeb451731af27cd2c8bbc06863f65a2b104
Marcelo Romulo Fernandes <persapiens@gmail.com>
2025-02-04 17:06:04 +0000

    feat: include router-outlet. crud will witch between insert and list components

--- src/app/owner-crud.component.ts
+++ src/app/owner-crud.component.ts
@@ -1,42 +1,16 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { OwnerInsertComponent } from "./owner-insert.component";
-import { OwnerListComponent } from './owner-list.component';
-import { PanelModule } from 'primeng/panel';
-import { Owner } from './owner';
-import { DividerModule } from 'primeng/divider';
-import { OwnerService } from './owner-service';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'owner-crud',
-  imports: [FormsModule, OwnerInsertComponent, OwnerListComponent, PanelModule, DividerModule],
+  imports: [FormsModule, RouterOutlet],
   template: `
-    <p-panel header="Owner">
-      <owner-insert (insertOutEvent)="insert($event)"></owner-insert>
+    <h2>Owners</h2>
 
-      <p-divider />
-
-      <owner-list (removeOutEvent)="remove($event)" [ownersList]="findAll()"></owner-list>
-    </p-panel>
+    <router-outlet />
   `,
   styleUrl: './app.component.scss'
 })
 export class OwnerCrudComponent {
-  ownerService: OwnerService
-
-  constructor(ownerService: OwnerService) {
-    this.ownerService = ownerService
-  }
-
-  insert(owner: Owner) {
-    this.ownerService.insert(owner)
-  }
-
-  remove(owner: Owner) {
-    this.ownerService.remove(owner.name)
-  }
-
-  findAll(): Array<Owner> {
-    return this.ownerService.findAll()
-  }
 }
