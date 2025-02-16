f33e03811137c5870f174f664df23a6578bca2a1
Marcelo Romulo Fernandes <persapiens@gmail.com>
2025-02-16 18:28:20 +0000

    feat: reorganize routers to login first

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,20 @@
 import { Routes } from '@angular/router';
 import { OwnerInsertComponent } from './owner-insert.component';
 import { OwnerListComponent } from './owner-list.component';
+import { LoginComponent } from './login.component';
+import { OwnerCrudComponent } from './owner-crud.component';
+import { AuthGuard } from './auth.guard';
 
 export const routes: Routes = [
-    { path: 'owners/new', component: OwnerInsertComponent },
-    { path: 'owners', component: OwnerListComponent},
-    { path: '', redirectTo: 'owners', pathMatch: 'full'},
-    { path: '**', redirectTo: 'owners'}
+    { path: 'login', component: LoginComponent },
+    {
+        path: 'owners', component: OwnerCrudComponent, canActivate: [AuthGuard],
+        children: [
+            { path: 'list', component: OwnerListComponent },
+            { path: 'new', component: OwnerInsertComponent },
+            { path: '', redirectTo: 'list', pathMatch: 'full' }
+        ]
+    },
+    { path: '', redirectTo: 'login', pathMatch: 'full'},
+    { path: '**', redirectTo: 'login'}
 ];
